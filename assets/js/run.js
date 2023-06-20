5cc1a9267572ade6832cf21873c9d4362df2b214
lucors <thesourcecode.max@yandex.ru>
2023-06-20 20:47:00 +0400

    Клиентская библиотека Socket.io перенесена в ресурсы клиента

--- assets/js/run.js
+++ assets/js/run.js
@@ -1,12 +1,3 @@
-//Подключение клиенсткой библиотеки Socket.io 
-function iolibPrepare(callback) {
-    let path = `${protocol}//${host}`;
-    if (!flags.debug){
-        path += "/iochatserver";
-    }
-    $.getScript(`${path}/socket.io/socket.io.min.js`, callback);
-}
-
 //-------------------------------------------------------------------------------------------------------
 // DOCUMENT READY EVENT
 //-------------------------------------------------------------------------------------------------------
@@ -14,8 +5,6 @@ $(document).ready(function(){
     if (!["http:", "https:", "ws:"].includes(protocol)) {
         protocol = "http:";
     }
-    iolibPrepare(() => {
-        mobilePrepare();
-        setStage("auth");
-    });
+    mobilePrepare();
+    setStage("auth");
 });
